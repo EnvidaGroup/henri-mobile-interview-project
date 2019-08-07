114e744b3bb002293f3da13b2fdaea8857c05eb3
EthanDM <ethandm@aol.com>
2019-08-07 12:22:01 -0700

    todos rendering, toggling status

--- src/screens/Todos.js
+++ src/screens/Todos.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, ActivityIndicator, FlatList } from 'react-native'
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  FlatList,
+  TouchableOpacity
+} from 'react-native'
+import FeatherIcons from 'react-native-vector-icons/Feather'
+
 import axios from 'axios'
 
 const Todos = () => {
@@ -15,7 +23,7 @@ const Todos = () => {
       const { data } = await axios.get(
         'https://jsonplaceholder.typicode.com/todos'
       )
-      console.log(data)
+
       setTodos(data)
       setLoading(false)
     } catch (error) {
@@ -23,24 +31,65 @@ const Todos = () => {
     }
   }
 
-  const renderUser = user => {
+  const setCompleted = todo => {
+    setTodos(prevState => {
+      const index = prevState.findIndex(x => x.id === todo.id)
+
+      if (index !== -1) {
+        prevState[index].completed = !prevState[index].completed
+      }
+
+      return [...prevState]
+    })
+  }
+
+  const renderUser = todo => {
     return (
-      <View>
-        <Text style={{ fontSize: 18 }}>{user.item.title}</Text>
-        <Text>{user.item.completed}</Text>
+      <View
+        style={{ marginVertical: 5, padding: 15, backgroundColor: 'white' }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              flexWrap: 'wrap',
+              flexDirection: 'row',
+              marginRight: 15
+            }}
+          >
+            <Text style={{ fontSize: 18, flex: 1 }}>{todo.item.title}</Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => setCompleted(todo.item)}
+            style={{ width: 32 }}
+          >
+            <FeatherIcons
+              size={28}
+              name={todo.item.completed ? 'check-circle' : 'circle'}
+              color={todo.item.completed ? 'green' : 'lightgray'}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 
   return (
-    <View>
+    <View style={{ flex: 1, backgroundColor: 'rgb(242, 242, 242)' }}>
       {loading ? (
         <ActivityIndicator style={{ marginTop: 20 }} size="large" />
       ) : (
         <FlatList
           data={todos}
           keyExtractor={user => user.id}
-          renderItem={user => renderUser(user)}
+          renderItem={renderUser}
         />
       )}
     </View>
